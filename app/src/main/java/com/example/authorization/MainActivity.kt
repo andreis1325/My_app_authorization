312fe4c551f9f4f5fa414a7f2d6e786ac231fab2
andreis1325 <andreis.90@mail.ru>
2020-12-31 22:09:51 +0300

    add choosing Sign up or Log in, keep logged in

--- app/src/main/java/com/example/authorization/MainActivity.kt
+++ app/src/main/java/com/example/authorization/MainActivity.kt
@@ -6,6 +6,7 @@ import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.BackgroundColorSpan
 import android.text.style.ForegroundColorSpan
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -19,9 +20,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         showLogo()
+        initOnClickListeners()
 
     }
 
+    private fun initOnClickListeners(){
+        vTvLogIn.setOnClickListener(){
+            vTvLogIn.setTextColor(resources.getColor(R.color.orange))
+            vTvSignUp.setTextColor(resources.getColor(R.color.white))
+            vLlRepeatPass.visibility = View.GONE
+            vFlLogIn.visibility = View.VISIBLE
+            vFlSignUp.visibility = View.GONE
+        }
+        vTvSignUp.setOnClickListener(){
+            vTvLogIn.setTextColor(resources.getColor(R.color.white))
+            vTvSignUp.setTextColor(resources.getColor(R.color.orange))
+            vLlRepeatPass.visibility = View.VISIBLE
+            vFlLogIn.visibility = View.GONE
+            vFlSignUp.visibility = View.VISIBLE
+        }
+
+        vIvKeepLogIn.setOnClickListener(){
+            if (vIvKeepLogIn.background.constantState?.equals(resources.getDrawable(R.drawable.right).constantState)!!)
+            vIvKeepLogIn.setBackgroundResource(R.drawable.wrong)
+            else vIvKeepLogIn.setBackgroundResource(R.drawable.right)
+        }
+
+    }
 
     private fun showLogo(){
         val spannableString = SpannableString(getString(R.string.logo))
